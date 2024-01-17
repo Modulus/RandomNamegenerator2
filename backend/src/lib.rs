9d7309e24b208babc0a753ad226aa40e8f69eb1b
Modulus <thejohnskauge@gmail.com>
2024-01-17 15:18:07 +0100

    Removed dead code

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -1,18 +1,2 @@
 
 pub mod names;
-use crate::names::RandonAnimalGenerator;
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
